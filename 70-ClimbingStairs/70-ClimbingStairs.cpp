e22a7b1c058a5e23c8a529cf1c73b66c255c6393
Sachin MS <72447101+laser069@users.noreply.github.com>
2026-03-26 13:27:38 +0530

    Create 70-ClimbingStairs

---
+++ 70-ClimbingStairs/70-ClimbingStairs.cpp
@@ -0,0 +1,14 @@
+// Last updated: 3/26/2026, 1:27:35 PM
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<=2)return n;
+        vector<int> ways(n+1);
+        ways[1] = 1;
+        ways[2] = 2;
+        for(int i = 3;i<=n;i++){
+        ways[i] = ways[i-1]+ways[i-2];
+        }
+        return ways[n];
+    }
+};

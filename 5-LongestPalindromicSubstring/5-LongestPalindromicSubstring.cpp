fdb2f6c9fcb05aaa97d2bc174208743de45d93ad
Sachin MS <72447101+laser069@users.noreply.github.com>
2026-03-26 14:45:08 +0530

    File updated at 3/26/2026, 2:45:04 PM

--- 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
+++ 5-LongestPalindromicSubstring/5-LongestPalindromicSubstring.cpp
@@ -1,34 +1,33 @@
-// Last updated: 3/26/2026, 1:28:10 PM
-class Solution {
-public:
-    string longestPalindrome(string s) {
-        int n = s.length();
-        int l,r;
-        int maxlen = 0;
-        string res;
-        for(int i =0;i<n;i++){
-            l = i-1;
-            r = i;
-            while(l>=0 && r<n && s[l] == s[r]){
-                if(r-l+1>maxlen){
-                    maxlen = r-l+1;
-                    res = s.substr(l,r-l+1);
-                }
-                r++;
-                l--;            
-            }
-            l = i;
-            r = i;
-            while(l>=0 && s[l] == s[r]){
-                if(r-l+1>maxlen){
-                    maxlen = r-l+1;
-                    res = s.substr(l,r-l+1);
-                }
-                r++;
-                l--;
-            }
-        }
-        return res;
-        
-    }
-};
+// Last updated: 3/26/2026, 2:45:04 PM
+1class Solution {
+2public:
+3    string longestPalindrome(string s) {
+4        int n = s.length();
+5        int resLen = 0;
+6        string res = "";
+7        for(int i = 0;i<n;i++){
+8            int l = i;
+9            int r = i;
+10            while(l>=0 && r<n &&s[r] == s[l]){
+11                if(resLen<r-l+1){
+12                    resLen = r-l+1;
+13                    res = s.substr(l,r-l+1);
+14                }
+15                r++;
+16                l--;
+17            }
+18            l = i;
+19            r = i+1;
+20            while(l>=0&&r<n&&s[r] == s[l]){
+21                if(resLen<r-l+1){
+22                    resLen = r-l+1;
+23                    res = s.substr(l,r-l+1);
+24                }
+25                l--;
+26                r++;
+27            }
+28        }
+29        return res;
+30        
+31    }
+32};

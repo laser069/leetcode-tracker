c2e58a393d08a72b5bef09c41f0592decb629407
Sachin MS <72447101+laser069@users.noreply.github.com>
2026-03-26 13:27:25 +0530

    Create 94-BinaryTreeInorderTraversal

---
+++ 94-BinaryTreeInorderTraversal/94-BinaryTreeInorderTraversal.cpp
@@ -0,0 +1,34 @@
+// Last updated: 3/26/2026, 1:27:22 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> result;
+        stack<TreeNode*> s;
+        TreeNode* curr = root;
+
+        while(curr != nullptr || !s.empty()){
+            while(curr!=nullptr){
+            s.push(curr);
+            curr= curr->left;
+            }
+
+            curr = s.top();
+            s.pop();
+            result.push_back(curr->val);
+            curr = curr->right;
+        } 
+        return result;
+
+    }
+};

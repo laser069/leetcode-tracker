086c4d8a0458767ff9ce7b7ba758f829b3ae1641
Sachin MS <72447101+laser069@users.noreply.github.com>
2026-03-30 15:13:47 +0530

    File updated at 3/30/2026, 3:13:45 PM

--- 605-CanPlaceFlowers/605-CanPlaceFlowers.cpp
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.cpp
@@ -1,19 +1,32 @@
-// Last updated: 3/26/2026, 1:26:29 PM
-class Solution {
-public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int size = flowerbed.size();
-        flowerbed.insert(flowerbed.begin(),0);
-        flowerbed.push_back(0);
-
-        for(int i =1;i<=size;i++){
-            if(flowerbed[i] == 0){
-                if(flowerbed[i-1]==0 && flowerbed[i+1] == 0){
-                    n--;
-                    flowerbed[i] = 1;
-                }
-            }
-        }
-    return n<=0;
-    }
-};
+// Last updated: 3/30/2026, 3:13:45 PM
+1class Solution {
+2public:
+3    bool isVowel(char c){
+4        char ch = tolower(c);
+5        return ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u';
+6    }
+7    string reverseVowels(string s) {
+8        int n = s.size();
+9        int l = 0;
+10        int r = n-1;
+11
+12        while(l<r){
+13            if(isVowel(s[l]) && isVowel(s[r])){
+14                char temp = s[l];
+15                s[l] = s[r];
+16                s[r] = temp;
+17                r--;
+18                l++;
+19            }
+20            else if(isVowel(s[l]) && !isVowel(s[r])){
+21                r--;
+22            }else if(isVowel(s[r]) && !isVowel(s[l])){
+23                l++;
+24            }else{
+25                r--;
+26                l++;
+27            }
+28        }
+29        return s;
+30    }
+31};
